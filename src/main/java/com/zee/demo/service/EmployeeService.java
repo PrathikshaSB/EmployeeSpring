d5921169ec44077af942aa1201a66832ad3a75c1
Prathiksha S B <prathiksha.sb@zee.com>
2022-04-29 11:47:15 +0530

    added logs

--- src/main/java/com/zee/demo/service/EmployeeService.java
+++ src/main/java/com/zee/demo/service/EmployeeService.java
@@ -1,8 +1,9 @@
 package com.zee.demo.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,15 +17,26 @@ import com.zee.demo.repository.EmployeeRepository;
 //updateEmployee
 //deleteEmployee
 
+
+
+
 @Service
 public class EmployeeService {
+	
+	
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	//its used only in this class so private, val wont change so final, Logger is the interface 
 	
 	@Autowired
 	private EmployeeRepository repository;
 	
 	public List<Employee> getAllEmployees(){
+		
 //		return new ArrayList<Employee>();
-		System.out.println("get employees");
+//		System.out.println("get employees");
+		
+		LOG.info("getAllEmployees");
+		
 		return repository.findAll();
 	}
 	
@@ -32,30 +44,56 @@ public class EmployeeService {
 	
 	public Employee getEmployeeById(int employeeId) {
 //		return null;
-		System.out.println("get employee "+employeeId);
-		return repository.findById(employeeId).get();
+//		System.out.println("get employee "+employeeId);
+		
+		LOG.info("getAllEmployeeById"+employeeId);
+		
+		
+		//return repository.findById(employeeId).get();
+		//findById return Optional obj, means it may or may not contain Employee obj
+		//if it exists, get() will execute, else, get() will fail
+		
+		
+		Optional<Employee> empOpt=repository.findById(employeeId);
+		Employee empObj;
+		try {
+			empObj = empOpt.get();
+			return empObj;
+		}
+		catch(NoSuchElementException e) {
+			LOG.warn(e.getMessage());
+			return null;
+		}
+		
+		
+		
 	}
 
 	public Employee addEmployee(Employee employee) {
 //		return null;
-		System.out.println("add employee");
+//		System.out.println("add employee");
+		LOG.info("addEmployee");
 		return repository.save(employee);
 	}
 	
 	public Employee updateEmployee(Employee employee) {
 //		return null;
-		System.out.println("upd employee");
+//		System.out.println("upd employee");
+		LOG.info("updateEmployees");
 		return repository.save(employee);
 	}
 	public Employee deleteEmployee(int employeeId) {
 //		return null;
 		Employee emp=repository.findById(employeeId).get();
-		System.out.println("del employee");
+//		System.out.println("del employee");
+		
+		LOG.info("deleteEmployee "+employeeId);
 		repository.delete(emp);
 		return emp;
 	}
 	public Employee getEmployeeByfn(String fn) {
-		System.out.println("get employee "+fn);
+//		System.out.println("get employee "+fn);
+		LOG.info("getEmployeeByFirstnAME"+fn);
 		return repository.findByFirstName(fn).get(0);
 	}
 	
